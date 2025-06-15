2d4249fb25ae0b6f2e95e6a95ac27213510dc37b
Raul <raulmirandavargas82@gmail.com>
2025-06-14 19:40:26 -0600

    Inicialización del input del chat

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextInput } from './components/text-input/text-input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
