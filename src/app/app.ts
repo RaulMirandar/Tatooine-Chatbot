71166d1176b50f0edc88e30da88f44fcae752d1c
JoseMontero09 <jopamon4@gmail.com>
2025-06-17 21:59:22 -0600

    Cascaron de la pagina

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'Tatooine-Chatbot';
+
+  // Estado del chatbot: visible o no
+  isChatbotHidden = true;
+
+  toggleChatbot() {
+    this.isChatbotHidden = !this.isChatbotHidden;
+  }
+
+  closeChatbot() {
+    this.isChatbotHidden = true;
+  }
 }
